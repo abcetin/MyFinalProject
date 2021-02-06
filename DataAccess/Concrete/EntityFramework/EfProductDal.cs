87f770b1c303cf5e5e794687095fbaa832db3867
TulparN <cetin.ab@outlook.com>
2021-02-07 00:57:56 +0300

    06/02/2021

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -52,6 +52,22 @@ namespace DataAccess.Concrete.EntityFramework
             }
         }
 
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var result = from p in context.Products
+                             join c in context.Categories
+                             on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto 
+                             {
+                                 ProductId = p.ProductId, ProductName = p.ProductName, 
+                                 CategoryName = c.CategoryName, UnitsInStock = p.UnitsInStock
+                             };
+                return result.ToList();
+            }
+        }
+
         public void Update(Product entity)
         {
             using (NorthwindContext context = new NorthwindContext())
